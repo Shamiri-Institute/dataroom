63b8757899b58ff629c30932c03180a14cb5bd07
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2025-07-27 13:47:59 +0300

    change the domain config

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -106,7 +106,7 @@ export const authOptions: NextAuthOptions = {
         sameSite: "lax",
         path: "/",
         // When working on localhost, the cookie domain must be omitted entirely (https://stackoverflow.com/a/1188145)
-        domain: VERCEL_DEPLOYMENT ? ".papermark.com" : undefined,
+        domain: VERCEL_DEPLOYMENT ? (process.env.NEXT_PUBLIC_BASE_URL ? `.${new URL(process.env.NEXT_PUBLIC_BASE_URL).hostname}` : undefined) : undefined,
         secure: VERCEL_DEPLOYMENT,
       },
     },
