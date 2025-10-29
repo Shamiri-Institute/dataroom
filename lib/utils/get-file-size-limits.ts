45186c204ee2960f18fabc5321a39331a71fddb4
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2025-10-29 10:37:55 +0300

    feat: remove file upload limits for shamiri (#11)

--- lib/utils/get-file-size-limits.ts
+++ lib/utils/get-file-size-limits.ts
@@ -16,30 +16,39 @@ export function getFileSizeLimits({
   isFree: boolean;
   isTrial: boolean;
 }): FileSizeLimits {
-  // Default limits based on plan type
-  const defaultLimits: FileSizeLimits = {
-    video: 500, // 500MB
-    document: isFree && !isTrial ? 100 : 350, // 100MB free, 350MB paid
-    image: isFree && !isTrial ? 30 : 100, // 30MB free, 100MB paid
-    excel: 40, // 40MB
-    maxFiles: 150,
-    maxPages: isFree && !isTrial ? 100 : 500,
-  };
+  // Default limits based on plan type as the papermark configuration
+  // const defaultLimits: FileSizeLimits = {
+  //   video: 500, // 500MB
+  //   document: isFree && !isTrial ? 100 : 350, // 100MB free, 350MB paid
+  //   image: isFree && !isTrial ? 30 : 100, // 30MB free, 100MB paid
+  //   excel: 40, // 40MB
+  //   maxFiles: 150,
+  //   maxPages: isFree && !isTrial ? 100 : 500,
+  // };
+
+  return {
+    video: 500,
+    document: 1000,
+    image: 1000,
+    excel: 40,
+    maxFiles: 500,
+    maxPages: 500,
+  } as FileSizeLimits;
 
   // If no custom limits are set, return default limits
-  if (!limits?.fileSizeLimits) {
-    return defaultLimits;
-  }
-
-  // Merge custom limits with defaults
-  return {
-    video: limits.fileSizeLimits.video ?? defaultLimits.video,
-    document: limits.fileSizeLimits.document ?? defaultLimits.document,
-    image: limits.fileSizeLimits.image ?? defaultLimits.image,
-    excel: limits.fileSizeLimits.excel ?? defaultLimits.excel,
-    maxFiles: limits.fileSizeLimits.maxFiles ?? defaultLimits.maxFiles,
-    maxPages: limits.fileSizeLimits.maxPages ?? defaultLimits.maxPages,
-  };
+  // if (!limits?.fileSizeLimits) {
+  //   return defaultLimits;
+  // }
+  //
+  // // Merge custom limits with defaults
+  // return {
+  //   video: limits.fileSizeLimits.video ?? defaultLimits.video,
+  //   document: limits.fileSizeLimits.document ?? defaultLimits.document,
+  //   image: limits.fileSizeLimits.image ?? defaultLimits.image,
+  //   excel: limits.fileSizeLimits.excel ?? defaultLimits.excel,
+  //   maxFiles: limits.fileSizeLimits.maxFiles ?? defaultLimits.maxFiles,
+  //   maxPages: limits.fileSizeLimits.maxPages ?? defaultLimits.maxPages,
+  // };
 }
 
 // Helper function to get size limit for a specific file type
